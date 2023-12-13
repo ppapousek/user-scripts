bf9959f4d1b7f25f2c8b9d4e4c895ed2282e2bf5
Petr Papousek <ppapousek@gmail.com>
2023-12-13 08:26:51 +0100

    update compact-outlook-notes.user.js - hide toolbars, just one line of note preview, expand window size

--- compact-outlook-notes.user.js
+++ compact-outlook-notes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Compact Outlook Notes
-// @namespace    http://tampermonkey.net/
-// @version      1
+// @namespace    https://github.com/ppapousek/user-scripts
+// @version      2
 // @description  Compact Outlook Notes
 // @author       ppapousek@gmail.com
 // @match        https://outlook.office.com/mail/notes
@@ -14,12 +14,56 @@
 
     let el = document.createElement('style');
     el.type = 'text/css';
-    el.innerText =
-            '.n-noteFocus .n-textBox { padding: 0 15px !important; }' +
-            '.n-editorContainer div.public-DraftEditor-content { line-height: 12.5px !important; font-size: 12px !important; }' +
-            '.n-detailViewContainer, .n-noteFocus { max-width: 1250px !important;}' +
-            '.n-left-pad, .n-padding { max-width: 15px !important; }' +
-            '.n-detailViewContainer { margin: 15px 0 15px !important; }';
+    // language=CSS
+    el.innerText = `
+        #LeftRail, #TopBar, .n-detailViewHeader, #appContainer > div[tabindex="-1"] {
+            display: none !important;
+        }
+
+        .rCmFE {
+            height: calc(100vh - 40px) !important;
+        }
+
+        .n-noteFocus {
+            max-height: 1500px !important;
+
+            .n-textBox {
+                padding: 0 15px !important;
+            }
+        }
+
+        .n-stickyNotePreview {
+            margin-top: -20px !important;
+
+            & p:nth-child(1) ~ * {
+                display: none !important;
+            }
+        }
+
+        .n-notePreviewContent {
+            padding-bottom: 5px !important;
+        }
+
+        .n-viewContainer {
+            margin-top: -50px !important;
+        }
+
+        .n-editorContainer div.public-DraftEditor-content {
+            line-height: 15px !important;
+            font-size: 14px !important;
+        }
+
+        .n-detailViewContainer, .n-noteFocus {
+            max-width: 1250px !important;
+        }
+
+        .n-left-pad, .n-padding {
+            max-width: 15px !important;
+        }
+
+        .n-detailViewContainer {
+            margin: 15px 0 15px !important;
+        }`;
 
     document.head.appendChild(el);
 })();
